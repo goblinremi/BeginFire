37e18c1ba7ff042e5e3a02f3565b1a750ff1fa19
Remi <pengremi@gmail.com>
2025-02-11 12:27:45 -0800

    did aal2 auth check in frontend and backend

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -8,9 +8,34 @@ export function withAuth<P extends object>(
         const supabase = await createClient();
 
         const { data, error } = await supabase.auth.getUser();
+
+        const { data: mfaData, error: mfaError } =
+            await supabase.auth.mfa.getAuthenticatorAssuranceLevel();
+
+        console.log("MFA DATA IS", mfaData);
+        console.log("MFA ERROR IS", mfaError);
+
         if (error || !data?.user) {
             redirect("/auth/login");
         }
+        if (mfaError) {
+            console.log("MFA ERROR IS", mfaError);
+            redirect("/auth/login");
+        }
+        if (mfaData) {
+            // if user has enrolled, but is not verified, redirect to verify page
+            if (
+                mfaData.nextLevel === "aal2" &&
+                mfaData.nextLevel !== mfaData.currentLevel
+            ) {
+                redirect("/auth/mfa/verify");
+            } else if (mfaData.nextLevel === "aal1") {
+                // if user has not enrolled, redirect to enrollment start
+                redirect("/auth/mfa/start");
+            }
+        }
+        // user is logged in and has MFA verified by this point
+
         const { data: profile } = await supabase
             .from("profiles")
             .select("onboarding_status")
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -39,13 +39,18 @@ export async function updateSession(request: NextRequest) {
         data: { user },
     } = await supabase.auth.getUser();
 
+    const { data: mfaData } =
+        await supabase.auth.mfa.getAuthenticatorAssuranceLevel();
+    console.log("MFA DATA from backend middleware IS", mfaData);
+
     console.log("request.nextUrl.pathname", request.nextUrl.pathname);
+    // These are the urls that are accessible without a user
     if (
         !user &&
         !request.nextUrl.pathname.startsWith("/auth/login") &&
         !request.nextUrl.pathname.startsWith("/auth/signup") &&
         !request.nextUrl.pathname.startsWith("/auth/email-not-confirmed") &&
-        !request.nextUrl.pathname.startsWith("/api/auth")
+        !request.nextUrl.pathname.startsWith("/api/auth/confirm")
     ) {
         // no user, potentially respond by redirecting the user to the login page
         console.log("REDIRECTING TO LOGIN BECAUSE USER IS NOT LOGGED IN");
@@ -53,6 +58,26 @@ export async function updateSession(request: NextRequest) {
         url.pathname = "/auth/login";
         return NextResponse.redirect(url);
     }
+    if (mfaData) {
+        // if i'm not on the mfa page, and i have mfa data, then i need to redirect to the mfa page
+        if (!request.nextUrl.pathname.startsWith("/auth/mfa")) {
+            if (
+                mfaData.nextLevel === "aal2" &&
+                mfaData.nextLevel !== mfaData.currentLevel
+            ) {
+                const url = request.nextUrl.clone();
+                url.pathname = "/auth/mfa/verify";
+                return NextResponse.redirect(url);
+            } else if (mfaData.nextLevel === "aal1") {
+                const url = request.nextUrl.clone();
+                url.pathname = "/auth/mfa/start";
+                return NextResponse.redirect(url);
+            }
+        }
+    }
+
+    // at this point, the user is logged in and has mfa verified
+
     if (user) {
         const { data: profile } = await supabase
             .from("profile")
@@ -69,12 +94,15 @@ export async function updateSession(request: NextRequest) {
             return NextResponse.redirect(url);
         }
         // if user is logged in and not in onboarding, and is on login or signup page, redirect to dashboard
-        //TODO: rework this logic for MFA
+        // also, if user is on the mfa page, and has aal2, redirect to the dashboard
         if (
             request.nextUrl.pathname.startsWith("/auth/login") ||
             request.nextUrl.pathname.startsWith("/auth/signup") ||
             request.nextUrl.pathname.startsWith("/auth/email-not-confirmed") ||
-            request.nextUrl.pathname.startsWith("/api/auth/confirm")
+            request.nextUrl.pathname.startsWith("/api/auth/confirm") ||
+            (mfaData?.nextLevel == "aal2" &&
+                mfaData?.currentLevel == "aal2" &&
+                request.nextUrl.pathname.startsWith("/auth/mfa"))
         ) {
             console.log("REDIRECTING TO DASHBOARD BECAUSE USER IS LOGGED IN");
             const url = request.nextUrl.clone();
